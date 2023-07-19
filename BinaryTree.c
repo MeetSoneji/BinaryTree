0bc237590fa0f545915f4c1f65270aa896d07c95
MeetSoneji <110390711+MeetSoneji@users.noreply.github.com>
2023-07-19 22:45:07 +0530

    Add files via upload

---
+++ BinaryTree.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+struct Node *createNode(int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    if (newNode == NULL)
+    {
+        printf("Memory allocation failed!\n");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+struct Node *insertNode(struct Node *root, int data)
+{
+    if (root == NULL)
+    {
+        return createNode(data);
+    }
+    else
+    {
+        if (data <= root->data)
+        {
+            root->left = insertNode(root->left, data);
+        }
+        else
+        {
+            root->right = insertNode(root->right, data);
+        }
+        return root;
+    }
+}
+void inorderTraversal(struct Node *root)
+{
+    if (root != NULL)
+    {
+        inorderTraversal(root->left);
+        printf("%d ", root->data);
+        inorderTraversal(root->right);
+    }
+}
+void preorderTraversal(struct Node *root)
+{
+    if (root != NULL)
+    {
+        printf("%d ", root->data);
+        preorderTraversal(root->left);
+        preorderTraversal(root->right);
+    }
+}
+void postorderTraversal(struct Node *root)
+{
+    if (root != NULL)
+    {
+        postorderTraversal(root->left);
+        postorderTraversal(root->right);
+        printf("%d ", root->data);
+    }
+}
+int main()
+{
+    struct Node *root = NULL;
+    root = insertNode(root, 50);
+    root = insertNode(root, 30);
+    root = insertNode(root, 20);
+    root = insertNode(root, 40);
+    root = insertNode(root, 70);
+    root = insertNode(root, 60);
+    root = insertNode(root, 80);
+    printf("Inorder traversal: ");
+    inorderTraversal(root);
+    printf("\n");
+    printf("Preorder traversal: ");
+    preorderTraversal(root);
+    printf("\n");
+    printf("Postorder traversal: ");
+    postorderTraversal(root);
+    printf("\n");
+    return 0;
+}
